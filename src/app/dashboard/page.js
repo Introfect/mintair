a8cff90d19b95513e4d5c5a3d1ca5a04e99bd5eb
Aayushman Pratap <76990626+Introfect@users.noreply.github.com>
2024-05-23 11:42:18 +0530

    useRouter dashoard build fix

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,17 +3,17 @@ import TransactionHistort from '@/components/TransactionHistort'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 const page = () => {
-  const router = useRouter()
-  useEffect(() => {
-    const data= localStorage.getItem('wagmi.recentConnectorId')
-    if(!data){
-      router.push("/")
-    }
-  }, [])
+  // const router = useRouter()
+  // useEffect(() => {
+  //   const data= localStorage.getItem('wagmi.recentConnectorId')
+  //   if(!data){
+  //     router.push("/")
+  //   }
+  // }, [])
 
   const getData = async ({ pageParam = 1 }) => {
     const response = await axios.get(`https://api.etherscan.io/api?module=account&action=txlistinternal&address=0x2c1ba59d6f58433fb1eaee7d20b26ed83bda51a3&startblock=0&endblock=2702578&page=${pageParam}&offset=10&sort=asc&apikey=CP2G1IC56U7WR35CZEJBE48SK8MGGHPH97`)
